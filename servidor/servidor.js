36393074239f78b4bfd2cdfcaf4291bf758be1ab
germancardenasm <germancardenas@gmail.com>
2018-09-19 23:03:50 -0500

    cargando peliculas al inicio

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -15,9 +15,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get("/peliculas?", controladorPeliculas.mostrarPeliculas);
-app.get("/generos", function(req, res){
+/* app.get("/generos", function(req, res){
   res.send("llego peticion de generos")
-  console.log("llego peticion de generos")});
+  console.log("llego peticion de generos")}); */
 //seteamos el puerto en el cual va a escuchar los pedidos la aplicación
 var puerto = '8080';
 
