a4b20cc78c5af7c146b3905e87e3f6a7fdcccb6d
germancardenasm <germancardenas@gmail.com>
2018-11-13 23:50:31 -0500

    Proyecto Entregado

--- scripts/CreateTables.sql
+++ scripts/CreateTables.sql
@@ -2,7 +2,6 @@
 -- En las dos primeras guias no guardé las sintaxis de la creacion de las tablas 
 --asi que tuve al final que usar el comando SHOW CREATE TABLE de las tablas "genero" y "Pelicula" para guardar aqui el script.
 
-
 CREATE TABLE `genero` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(30) NOT NULL,
@@ -25,17 +24,17 @@ CREATE TABLE `pelicula` (
   CONSTRAINT `pelicula_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `genero` (`id`)
 ) 
 
-CREATE TABLE actor
+CREATE TABLE `actor`
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    nombre VARCHAR(70) NOT NULL,  
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    `nombre` VARCHAR(70) NOT NULL,  
 );
 
 CREATE TABLE actor_pelicula
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    actor_id int NOT NULL,
-    pelicula_id int NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `actor_id` int NOT NULL,
+    `pelicula_id` int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (actor_id) REFERENCES actor(id),
     FOREIGN KEY (pelicula_id) REFERENCES  pelicula(id)
